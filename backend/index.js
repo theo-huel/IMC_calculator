bd6a63169fd28fdb0157a62c2abec41ac3f15ad3
EL AHDAL Mohamed <melahdal@Simons-MacBook-Air.local>
2025-09-18 10:19:34 -0400

    Db linked + routes ready

--- backend/index.js
+++ backend/index.js
@@ -3,46 +3,62 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./firebase-service-key.json'); // ton fichier clé Firebase
+const serviceAccount = require('./firebase-service-key.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://imc-app-c6609-default-rtdb.firebaseio.com'
+  credential: admin.credential.cert(serviceAccount)
 });
 
-const db = admin.database();
+const db = admin.firestore();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-app.post('/calculate', async (req, res) => {
-  const { poids, taille, userId } = req.body;
 
-  if (!poids || !taille) return res.status(400).send("Valeurs invalides");
-
-  const imc = poids / (taille * taille);
-  let interpretation = '';
-
-  if (imc < 18.5) interpretation = 'Insuffisance pondérale';
-  else if (imc < 25) interpretation = 'Normal';
-  else if (imc < 30) interpretation = 'Surpoids';
-  else interpretation = 'Obésité';
-//tegxud
-  // Sauvegarde dans Firebase Realtime Database
-  if (userId) {
-    await db.ref('calculsIMC').push({
-      userId,
-      poids,
-      taille,
-      imc: imc.toFixed(2),
-      interpretation,
-      date: new Date().toISOString()
-    });
-  }
-
-  res.json({ imc: imc.toFixed(2), interpretation });
+// tester le serveur 
+app.get('/', (req, res) => {
+  res.send('Serveur opérationnel');
 });
 
-const PORT = 3001;
+//Afficher tt les utilisateurs sur la Db
+app.get('/users', async (req, res) => {
+  try {
+    const snapshot = await db.collection('calculsIMC').get();
+    const users = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    res.json(users);
+  } catch (error) {
+    res.status(500).send('Erreur lors de la récupération des utilisateurs');
+  }
+});
+
+app.post('/saveDb', async (req, res) => {
+  const { FirstName, LastName, imc, interpretation} = req.body;
+  try {
+    const newDoc = await db.collection('calculsIMC').add({
+      FirstName,
+      LastName,
+      imc,
+      interpretation,
+    });
+    res.status(201).json({ id: newDoc.id });
+  } catch (error) {
+    res.status(500).send('Erreur lors de la sauvegarde des données');
+  }
+});
+
+// Route pour récupérer les calculs IMC depuis Firestore
+app.get('/calculs', async (req, res) => {
+  try {
+    const snapshot = await db.collection('calculsIMC').get();
+    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    res.json(data);
+  } catch (error) {
+    res.status(500).send('Erreur lors de la récupération des données');
+  }
+});
+
+
+
+const PORT = 3009;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
